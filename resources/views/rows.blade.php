34eee5994769f3ed2fba1c7e7ab250d700c5e2a1
feras <ferasshaher2013@gmail.com>
2024-08-14 14:32:38 +0300

    fix blades

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -19,13 +19,12 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
-                                {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                            @foreach ($users as $index => $user)
+                                <tr class="{{ $index % 2 == 1 ? 'bg-red-100' : '' }}">
+                                    <td>{{ $loop->iteration }}</td> {{-- This will display the row number --}}
                                     <td>{{ $user->name }}</td>
-                                    {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="{{ $loop->first ? 'font-bold' : '' }}">{{ $user->email }}</td>
+                                    {{-- Only the first row has "font-bold" --}}
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach
